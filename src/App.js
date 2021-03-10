0fdc105b484c3e96b2598be2066eece42d7de89d
ahnafabir <ahnafabir313@gmail.com>
2021-03-10 22:14:23 +0600

    added header and logo

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +7,21 @@ import {
 import './App.css';
 import Home from './Components/Home/Home';
 import TeamDetails from "./Components/TeamDetails/TeamDetails";
+import { GiCrestedHelmet } from 'react-icons/gi';
 
 
 function App() {
   return (
     <div className="App">
       <Router >
+        <AppBar position="sticky" style={{ background: '#5FAD64' }}>
+          <Toolbar>
+            <Typography variant="h5">
+              <GiCrestedHelmet style={{fontSize: 'xx-large'}}/> League Of Legends
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
         <Switch>
           <Route exact path="/">
             <Home />
