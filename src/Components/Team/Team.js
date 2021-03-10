8e06f76d7a5eae72393c77ed07096cdf9b441c2c
ahnafabir <ahnafabir313@gmail.com>
2021-03-10 20:39:44 +0600

    use history removed

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -3,14 +3,15 @@ import { Card } from 'react-bootstrap';
 import Button from '@material-ui/core/Button'
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import './Team.css';
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Team = ({team})=>  {
   const {idTeam, strTeamBadge, strTeam, strSport, strGender} = team;
-  const history = useHistory();
-  const handleClick = ()=> {
-    history.push(`/details/${idTeam}`)
-  }
+  // const history = useHistory();
+  // const handleClick = ()=> {
+  //   history.push(`/details/${idTeam}`)
+  // }
 
   return (
     <div className="Team">
@@ -19,15 +20,18 @@ const Team = ({team})=>  {
         <Card.Body>
           <Card.Title className="text-center">{strTeam}</Card.Title>
           <Card.Text className="text-center">{strSport} || {strGender}</Card.Text>
+
+          <Link to={`/details/${idTeam}`}>
           <Button
             variant="contained"
             color="primary"
             endIcon={<SearchOutlinedIcon />}
             className="m-auto d-flex"
-            onClick={handleClick}
+            // onClick={handleClick}
           >
             Explore
           </Button>
+          </Link>
         </Card.Body>
       </Card>
     </div>
