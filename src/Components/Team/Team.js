3d2091723c370da685be92b9c11143758d43241a
ahnafabir <ahnafabir313@gmail.com>
2021-03-10 20:55:26 +0600

    readme website link edited

--- src/Components/Team/Team.js
+++ src/Components/Team/Team.js
@@ -3,15 +3,14 @@ import { Card } from 'react-bootstrap';
 import Button from '@material-ui/core/Button'
 import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import './Team.css';
-// import { useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router';
 
 const Team = ({team})=>  {
   const {idTeam, strTeamBadge, strTeam, strSport, strGender} = team;
-  // const history = useHistory();
-  // const handleClick = ()=> {
-  //   history.push(`/details/${idTeam}`)
-  // }
+  const history = useHistory();
+  const handleClick = ()=> {
+    history.push(`/details/${idTeam}`)
+  }
 
   return (
     <div className="Team">
@@ -21,17 +20,15 @@ const Team = ({team})=>  {
           <Card.Title className="text-center">{strTeam}</Card.Title>
           <Card.Text className="text-center">{strSport} || {strGender}</Card.Text>
 
-          <Link to={`/details/${idTeam}`}>
           <Button
             variant="contained"
             color="primary"
             endIcon={<SearchOutlinedIcon />}
             className="m-auto d-flex"
-            // onClick={handleClick}
+            onClick={handleClick}
           >
             Explore
           </Button>
-          </Link>
         </Card.Body>
       </Card>
     </div>
